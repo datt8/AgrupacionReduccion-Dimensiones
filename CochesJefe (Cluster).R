d3fa54e0b90d57d54587c9172473d76a1c057d65
datt8 <daniel.tome@cunef.edu>
2018-12-17 17:26:34 +0100

    Acutalización del archivo

--- CochesJefe (Cluster).R
+++ CochesJefe (Cluster).R
@@ -4,7 +4,7 @@
 ## Lectura de los datos del archivo
 
 library(foreign)
-data.original <- data.frame(read.spss("tterreno.sav"))
+data.original <- data.frame(read.spss("Datos/tterreno.sav"))
 
 ## Estructura de los datos
 summary(data.original) # Se observa que existen varias variables que tienen valores ausentes (NA)
@@ -23,7 +23,9 @@ data <- data.frame(data[,-14], row.names = data[,14]) # Se coloca la nueva varia
 data$cilindro <- as.numeric(as.character(data$cilindro))
 data$plazas <- as.numeric(as.character(data$plazas))
 str(data) # Queda una variable factor, acel2, que no se cambiará pues será eliminada para el análisis
+summary(data$acel2) # Casi todos están en la misma categoría
 data <- data[,-13]
+data$pvp <- round(data$pvp/166.386, -2) # Se pasa a euros y se redondea a la centena
 
 ## Correlación
 
@@ -40,37 +42,37 @@ library(tidyr)
 library(dplyr)
 summary(data.def) # Existen valores ausentes en: peso, cons90, consurb y velocida
 
-  ### Ausentes de peso
-  data.def$peso <- replace_na(data.def$peso, 1800) # Tras ver el dataset se determina que un posible peso podría ser 1800, pues esos dos coches son muy parecidos a los otros Ford Maverick (aunque con menos valor en las variables)
+### Ausentes de peso
+data.def$peso <- replace_na(data.def$peso, 1800) # Tras ver el dataset se determina que un posible peso podría ser 1800, pues esos dos coches son muy parecidos a los otros Ford Maverick (aunque con menos valor en las variables)
 
-  ### Ausentes de consumo 90
-  cons90_marca <- data.original %>%
-    group_by(marca) %>%
-    dplyr::summarize(media90 = mean(cons90, na.rm = TRUE))
+### Ausentes de consumo 90
+cons90_marca <- data.original %>%
+  group_by(marca) %>%
+  dplyr::summarize(media90 = mean(cons90, na.rm = TRUE))
 
-  data.def$cons90.2 <- ifelse(test = (data.original$marca == "NISSAN" & is.na(data.original$cons90)), yes = 8.4, no = data.original$cons90) # Se sustituye por la media de la marca
-  data.def$cons90.3 <- ifelse(test = (data.original$marca == "SSANGYONG" & is.na(data.original$cons90)), yes = 8.17, no = data.def$cons90.2) # Se sustituye por la media de la marca
-  data.def$cons90.4 <- ifelse(test = (data.original$marca == "UAZ" & is.na(data.original$cons90)), yes = 8, no = data.def$cons90.3) # Se observan el SSANGYONG Family RV (muy similar a los UAZ) y se determina poner 8 litros
+data.def$cons90.2 <- ifelse(test = (data.original$marca == "NISSAN" & is.na(data.original$cons90)), yes = 8.4, no = data.original$cons90) # Se sustituye por la media de la marca
+data.def$cons90.3 <- ifelse(test = (data.original$marca == "SSANGYONG" & is.na(data.original$cons90)), yes = 8.17, no = data.def$cons90.2) # Se sustituye por la media de la marca
+data.def$cons90.4 <- ifelse(test = (data.original$marca == "UAZ" & is.na(data.original$cons90)), yes = 8, no = data.def$cons90.3) # Se observan el SSANGYONG Family RV (muy similar a los UAZ) y se determina poner 8 litros
 
-  data.def$cons90 <- round(data.def$cons90.4, 1)
-  data.def[, 11:13] <- NULL
+data.def$cons90 <- round(data.def$cons90.4, 1)
+data.def[, 11:13] <- NULL
 
-  ### Ausentes consumo urbano
+### Ausentes consumo urbano
 
-  data.def$consurb.2 <- ifelse(test = (data.original$marca == "JEEP" & is.na(data.original$consurb)), yes = 9.8, no = data.original$consurb) # Se sustituye por el mismo valor que la version JAMB
-  data.def$consurb.3 <- ifelse(test = (data.original$marca == "NISSAN" & is.na(data.original$consurb)), yes = 12, no = data.def$consurb.2) # Tras observar todos los NISSAN se determina ese valor (Terrano 11.6 y Patrol 12.4)
-  data.def$consurb.4 <- ifelse(test = (data.original$marca == "TOYOTA" & is.na(data.original$consurb)), yes = 10.4, no = data.def$consurb.3) # Se sustituye por el valor del RAV4 5 puertas (muy similar)
+data.def$consurb.2 <- ifelse(test = (data.original$marca == "JEEP" & is.na(data.original$consurb)), yes = 9.8, no = data.original$consurb) # Se sustituye por el mismo valor que la version JAMB
+data.def$consurb.3 <- ifelse(test = (data.original$marca == "NISSAN" & is.na(data.original$consurb)), yes = 12, no = data.def$consurb.2) # Tras observar todos los NISSAN se determina ese valor (Terrano 11.6 y Patrol 12.4)
+data.def$consurb.4 <- ifelse(test = (data.original$marca == "TOYOTA" & is.na(data.original$consurb)), yes = 10.4, no = data.def$consurb.3) # Se sustituye por el valor del RAV4 5 puertas (muy similar)
 
-  data.def$consurb <- round(data.def$consurb.4, 1)
-  data.def[, 11:13] <- NULL
+data.def$consurb <- round(data.def$consurb.4, 1)
+data.def[, 11:13] <- NULL
 
-  ### Ausentes velocidad
+### Ausentes velocidad
 
-  data.def$velocida.2 <- ifelse(test = (data.original$marca == "TATA" & is.na(data.original$velocida)), yes = 135, no = data.original$velocida) # Mismo valor que el otro TATA (igual que el ausente)
-  data.def$velocida.3 <- ifelse(test = (data.original$marca == "SUZUKI" & is.na(data.original$velocida)), yes = 146, no = data.def$velocida.2) # Se sustituye por valor intermedio entre otros SUZUKI de 95 CV
+data.def$velocida.2 <- ifelse(test = (data.original$marca == "TATA" & is.na(data.original$velocida)), yes = 135, no = data.original$velocida) # Mismo valor que el otro TATA (igual que el ausente)
+data.def$velocida.3 <- ifelse(test = (data.original$marca == "SUZUKI" & is.na(data.original$velocida)), yes = 146, no = data.def$velocida.2) # Se sustituye por valor intermedio entre otros SUZUKI de 95 CV
 
-  data.def$velocida <- data.def$velocida.3
-  data.def[, 11:12] <- NULL
+data.def$velocida <- data.def$velocida.3
+data.def[, 11:12] <- NULL
 
 ## Comprobación de NA'S
 apply(X = data.def, MARGIN = 2, FUN = function(x){sum(is.na(x))}) # No hay NA'S ya
@@ -79,6 +81,7 @@ apply(X = data.def, MARGIN = 2, FUN = function(x){sum(is.na(x))}) # No hay NA'S 
 
 # Aproximación (gráfico y datos escalados)
 
+library(factoextra)
 data.scale <- data.frame(scale(data.def)) # Se escalan los datos para eliminar las unidades de medida
 dist.eucl <- get_dist(x = data.scale, method = "euclidean") # Se hallan las distancias entre los coches
 fviz_dist(dist.obj = dist.eucl, lab_size = 5, 
@@ -88,14 +91,13 @@ fviz_dist(dist.obj = dist.eucl, lab_size = 5,
 
 ## Clusters óptimos gráficos
 
-library(factoextra)
-fviz_nbclust(data.scale, hcut, method = "wss") +
+plot.j <- fviz_nbclust(data.scale, hcut, method = "wss") +
   geom_vline(xintercept = 2, linetype = 2) +
   geom_vline(xintercept = 3, linetype = 3) +
   ggtitle("Número óptimo de clusters - jerárquico") +
   labs(x = "Número k de clusters", y = "Suma total de cuadrados intra grupos") # Gráfica para determinar el número óptimo de clusters (3)
 
-fviz_nbclust(data.scale, kmeans, method = "wss") +
+plot.k <- fviz_nbclust(data.scale, kmeans, method = "wss") +
   geom_vline(xintercept = 2, linetype = 2) +
   geom_vline(xintercept = 3, linetype = 3) +
   ggtitle("Número óptimo de clusters - k medias") +
@@ -106,9 +108,6 @@ fviz_nbclust(data.scale, kmeans, method = "wss") +
 library(NbClust)
 clusters.optimos <- NbClust(data = data.scale, distance = "euclidean", min.nc = 2, 
                             max.nc = 10, method = "complete", index = "all") # Por regla mayoritaria se deberían elegir 3
-
-tt.clus.optim <- data.def # Se hace una copia del data scale para el siguiente paso
-clusters.optimos$Best.partition -> tt.clus.optim$cluster # Se incluye una nueva columna en el data frame con el cluster correspondiente
 
 ## ¿Es necesario el clustering? Estadístico Hopkins
 
@@ -124,7 +123,8 @@ hopkins(data = data.scale, n = nrow(data.scale) - 1) # Como es cercano a 0 (esta
 
 library(cluster)
 cluster.jerarquico <- hclust(dist.eucl, method = "ward.D")
-plot(cluster.jerarquico, cex = 0.6, hang = -1)
+plot(cluster.jerarquico, cex = 0.45, ylab = "", xlab = "", axes = FALSE, ann = FALSE) + 
+  title(main = "Dendrograma")
 rect.hclust(cluster.jerarquico, k = 3, border = 2:4) #Se cogen los 3 clusters
 
 data.def -> tt.clus.j # Se crea un data frame para añadir la columna del cluster resultante
@@ -140,4 +140,33 @@ tt.clus.kmeans$cluster <- cluster.kmeans$cluster
 
 
 # Fase 4. Análisis de cada cluster
+
+# Se crea para cada tipo de clusterización usada (jerárquica y no jerárquica, una tabla que con sus estadísticos)
+ch.clusters.j <- tt.clus.j %>%
+  group_by(cluster) %>%
+  dplyr::summarize(precio = mean(pvp),
+                   cilindros = median(cilindro),
+                   cilindrada = mean(cc),
+                   potencia = mean(potencia),
+                   rpm = mean(rpm),
+                   peso = mean(peso),
+                   plazas = median(plazas),
+                   consumo90 = mean(cons90),
+                   consumourbano = mean(consurb),
+                   velocidad = mean(velocida),
+                   elementos = n())
+
+ch.clusters.kmeans <- tt.clus.kmeans %>%
+  group_by(cluster) %>%
+  dplyr::summarize(precio = mean(pvp),
+                   cilindros = median(cilindro),
+                   cilindrada = mean(cc),
+                   potencia = mean(potencia),
+                   rpm = mean(rpm),
+                   peso = mean(peso),
+                   plazas = median(plazas),
+                   consumo90 = mean(cons90),
+                   consumourbano = mean(consurb),
+                   velocidad = mean(velocida),
+                   elementos = n())
 
